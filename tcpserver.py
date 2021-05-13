f04b709b387c3220d0e75d545afc6ea4f9a146f8
Guilherme Matera <77033006+guimatera@users.noreply.github.com>
2021-05-13 11:08:13 -0300

    Update tcpserver.py

--- tcpserver.py
+++ tcpserver.py
@@ -7,7 +7,7 @@ server_socket.bind(['', server_port])
 # Nessa parte, o programa 'escuta' as requisições do cliente. 1 é o numero máximo de conexoes em fila.
 server_socket.listen(1)
 print ('The server is ready to receive')
-# Loop para manter servidor pronto para receber/enviar menssagens para algum cliente
+# Loop para manter servidor pronto para receber/enviar mensagens para algum cliente
 while 1:
     connection_socket, addr = server_socket.accept()
     sentence = connection_socket.recv(2048)
