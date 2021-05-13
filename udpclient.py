b546f7f52b9bf33b7cbf6ca0a53f1accc653ac8d
Guilherme Matera <77033006+guimatera@users.noreply.github.com>
2021-05-13 14:45:23 -0300

    Update udpclient.py

--- udpclient.py
+++ udpclient.py
@@ -2,13 +2,13 @@ from socket import *
 
 # Nome ou endereço IP e numero da porta socket do servidor 
 server_name = 'server_name'
-server_port = 2021
+server_port = 8000
 # Criando socket chamado client_socket com endereço tipo IPV4 e protocolo UDP
-client_socket = socket(socket.AF_INET, socket.SOCK_DGRAM)
+client_socket = socket(AF_INET, SOCK_DGRAM)
 # Aplicacao...
 message = input('Input lowercase sentence: ')
 # Enviando mensagem ao servidor buscando endereço e porta socket
-client_socket.sendto(message,(server_name, server_port))
+client_socket.sendto(message.encode(),(server_name, server_port))
 # Recebimento da resposta do servidor mais seu endereço (do qual não necessitamos, pois já o temos)
 modifiedMessage, serverAddress = client_socket.recvfrom(2048)
 print (modifiedMessage)
